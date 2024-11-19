316b7e8959c47639a1454f48926ad1380688c88c
Juanma <juanma7685@gmail.com>
2024-11-19 02:13:32 +0100

    hola

--- models/staging/fecha/fecha.sql
+++ models/staging/fecha/fecha.sql
@@ -8,13 +8,21 @@ WITH my_cte AS (
 
 SELECT 
     date_hour,
+    CAST(date_hour AS DATE) AS date, -- Nueva columna para la fecha sin la hora
+    {{ dbt_utils.surrogate_key(['date_hour']) }} AS id_fecha_hora, -- Clave subrrogada con fecha y hora
+    {{ dbt_utils.surrogate_key(['CAST(date_hour AS DATE)']) }} AS id_fecha, -- Clave subrrogada sin la hora
     EXTRACT(YEAR FROM date_hour) AS year,
     EXTRACT(MONTH FROM date_hour) AS month,
     EXTRACT(DAY FROM date_hour) AS day,
     EXTRACT(HOUR FROM date_hour) AS hour,
     EXTRACT(WEEK FROM date_hour) AS week,
     EXTRACT(DOW FROM date_hour) AS day_of_week, -- Día de la semana (0=Lunes, 6=Domingo)
-    EXTRACT(DOY FROM date_hour) AS day_of_year -- Día del año (1-365 o 1-366 en años bisiestos)
+    EXTRACT(DOY FROM date_hour) AS day_of_year, -- Día del año (1-365 o 1-366 en años bisiestos)
+    EXTRACT(QUARTER FROM date_hour) AS quarter,
+    CASE 
+        WHEN EXTRACT(DOW FROM date_hour) IN (0, 6) THEN 'Weekend'
+        ELSE 'Weekday'
+    END AS day_type
 FROM my_cte
 
 
