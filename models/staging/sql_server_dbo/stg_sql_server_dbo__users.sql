f98e6fbbdf1239e6690bf0270bb3ec2fc2a6630d
Juanma <juanma7685@gmail.com>
2024-11-19 13:08:27 +0100

    fechas

--- models/staging/sql_server_dbo/stg_sql_server_dbo__users.sql
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__users.sql
@@ -10,10 +10,10 @@ renamed as (
 
     select
         user_id,
-        updated_at,
+        {{ dbt_utils.generate_surrogate_key(['updated_at']) }} as id_updated_at,
         address_id,
         last_name,
-        created_at,
+        {{ dbt_utils.generate_surrogate_key(['created_at']) }} as id_created_at,
         phone_number,
         first_name,
         email,
