a3a25fc39e1e06f4a297b5bc633b932c548ac071
Juanma <juanma7685@gmail.com>
2024-11-20 17:34:44 +0100

    Arreglar silver

--- models/staging/sql_server_dbo/stg_sql_server_dbo__orders.sql
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__orders.sql
@@ -7,6 +7,8 @@ source as (
 renamed as (
     select
         order_id,
+        address_id,
+        user_id,
         shipping_cost,
         CAST(created_at AS DATE) as created_at,
         CAST(estimated_delivery_at AS DATE) as estimated_delivery_at,
@@ -14,6 +16,9 @@ renamed as (
         order_total,
         CAST(delivered_at AS DATE) as delivered_at,
         tracking_id,
+        {{ dbt_utils.generate_surrogate_key(["COALESCE(NULLIF(shipping_service, ''), 'Waiting')"]) }} as shipping_service_id,
+        {{ dbt_utils.generate_surrogate_key(["COALESCE(NULLIF(promo_id, ''), 'No Promo')"]) }} as promo_id,
+        {{ dbt_utils.generate_surrogate_key(['status']) }} as status_id,
         _fivetran_deleted,
         _fivetran_synced
     from source 
