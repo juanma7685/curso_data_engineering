1d55171d8d80e69a94244d7aea6ccc587f4df72e
Juanma <juanma7685@gmail.com>
2024-11-29 23:04:18 +0100

    Muchas metricas e incrementales donne

--- models/staging/sql_server_dbo/stg_sql_server_dbo__addresses.sql
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__addresses.sql
@@ -7,11 +7,13 @@
 
 with 
 
+-- 1. Fuente de datos
 source as (
-    select * 
+    select distinct *
     from {{ source('sql_server_dbo', 'addresses') }}
 ),
 
+-- 2. Datos renombrados
 renamed as (
     select
         {{ dbt_utils.generate_surrogate_key(["address", "zipcode", "country", "state"]) }} as address_id,
@@ -25,9 +27,10 @@ renamed as (
     from source
 ),
 
+-- 3. Dirección predeterminada
 no_addresses as (
-    select
-        {{ dbt_utils.generate_surrogate_key(["null","null","null","null"]) }} as address_id,
+    select distinct
+        {{ dbt_utils.generate_surrogate_key(["'no address'", "'no address'", "'no address'", "'no address'"]) }} as address_id,
         null as zipcode,
         'no address' as country,
         'no address' as address,
@@ -35,14 +38,24 @@ no_addresses as (
         null as street,
         null as state,
         '0' as llegada_id
+),
+
+-- 4. Deduplicación y manejo de conflictos
+deduplicated as (
+    select *, 
+           row_number() over (partition by address_id order by llegada_id desc) as row_num
+    from (
+        select * from renamed
+        union all
+        select * from no_addresses
+    )
 )
 
-select * from renamed
-
-union all
-
-select * from no_addresses
+-- 5. Selección final
+select *
+from deduplicated
+where row_num = 1
 
 {% if is_incremental() %}
-where llegada_id > (select max(llegada_id) from {{ this }})
+and llegada_id > (select max(llegada_id) from {{ this }})
 {% endif %}
